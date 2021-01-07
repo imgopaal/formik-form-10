9bcaee3403d5d8be36cc271c19dda42d1a858d5a
GopalDas123 <Gopaldax@gmail.com>
2021-01-07 21:36:20 +0500

    Working Fine

--- src/components/zAge/Age.tsx
+++ src/components/zAge/Age.tsx
@@ -1,7 +1,5 @@
 // import React, { useState } from "react";
-// import DatePicker from "react-datepicker";
 // import RadioButton from "../Radio";
-// import "react-datepicker/dist/react-datepicker.css";
 // import "./age.css";
 // import { Formik, Field, Form, ErrorMessage } from "formik";
 // import * as Yup from "yup";
@@ -23,13 +21,6 @@
 //         >
 //         </Formik>
 
-//         <span>Date of birth </span>
-
-//         <DatePicker
-//           placeholderText="MM-DD-YYYY"
-//           selected={selectedDate}
-//           onChange={(date: any) => setSelectedDate(date)}
-//           />
 //         <RadioButton />
 //         <button type="submit">Sign Up</button>
 //       </div>
@@ -38,22 +29,38 @@
 // };
 // export default Age;
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Formik, Field, Form } from 'formik';
+import React, { useState } from "react";
+import { Formik, Field, Form } from "formik";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import Swal from "sweetalert2";
 
-export const Age=({submit}:any)=> (
-  <div>
-    <Formik
-      initialValues={{
-        picked: '',
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        submit(0);
-      }}
-    >
-      {({ values }) => (
+// export const Age = ({ submit }: any) => (
+//   //  const [selectedDate, setSelectedDate]= useState(null);
+//   return(
+
+// );
+
+export const Age = ({ submit }: any) => {
+  const [selectedDate, setSelectedDate] = useState(null);
+  return (
+    <div>
+      <Formik
+        initialValues={{
+          picked: "",
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          Swal.fire("Sign up Successful", "You can login now", "success");
+          submit(0);
+        }}
+      >
         <Form>
+          <span>Date of birth </span>
+          <DatePicker
+            placeholderText="MM-DD-YYYY"
+            selected={selectedDate}
+            onChange={(date: any) => setSelectedDate(date)}
+          />
           <div role="group" aria-labelledby="my-radio-group">
             <label>
               <Field type="radio" name="picked" value="Male" />
@@ -67,8 +74,7 @@ export const Age=({submit}:any)=> (
 
           <button type="submit">Submit</button>
         </Form>
-      )}
-    </Formik>
-  </div>
-);
-
+      </Formik>
+    </div>
+  );
+};
